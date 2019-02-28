f4d581efc4fe8c23aaccad42ead47460b186dd5f
Waret Li <waret87@gmail.com>
2019-02-28 20:22:47 +0800

    update

--- scripts/case.sh
+++ scripts/case.sh
@@ -6,3 +6,11 @@ sh -x test.sh api=/api/admin -- parameter-a:claim-value
 sh -x test.sh username=alice api=/api/admin -- parameter-a:claim-value
 sh -x test.sh username=alice password=alice api=/api/admin -- parameter-a:claim-value
 
+sh -x scripts/test.sh username=admin api=/album api_host=http://localhost:8081
+
+
+sh -x scripts/test.sh username=admin api=/admin/album api_host=http://localhost:8081 rpt=true res_client=resource-photoz
+sh -x scripts/test.sh username=alice api=/admin/album api_host=http://localhost:8081 rpt=true res_client=resource-photoz
+sh -x scripts/test.sh username=alice api=/admin/album api_host=http://localhost:8081
+sh -x scripts/test.sh username=admin api=/admin/album api_host=http://localhost:8081
+
