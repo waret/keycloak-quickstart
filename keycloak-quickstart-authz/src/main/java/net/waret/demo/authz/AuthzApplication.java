0d96164818cc72a98a6071048039f2500eb0a29b
Waret Li <waret87@gmail.com>
2019-03-15 16:03:07 +0800

    update

---
+++ keycloak-quickstart-authz/src/main/java/net/waret/demo/authz/AuthzApplication.java
@@ -0,0 +1,16 @@
+package net.waret.demo.authz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class AuthzApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(AuthzApplication.class, args);
+	}
+
+}
