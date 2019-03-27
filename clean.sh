266358c08dc8a457a4e54426ca263c8f35ab84b2
Waret Li <waret87@gmail.com>
2019-03-27 10:14:41 +0800

    update

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 
 docker-compose rm -s -f
 docker volume rm keycloak-quickstart_keycloak-datastore
-docker network rm keycloak-quickstart_deepmesh-net
+docker network rm keycloak-quickstart_keycloak-quickstart
